90567c835dd816ddd8e430038b9a52221c7427c2
lgthle <luis.guethle@googlemail.com>
2012-11-25 14:20:32 +0100

    added register button to GUI
    Signed-off-by: lgthle <luis.guethle@googlemail.com>

---
+++ entwicklung/workspace/SherlockHolmesSystems/src/VIEW/Main.java
@@ -0,0 +1,31 @@
+package VIEW;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Main extends JFrame {
+	public Main() {
+	}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	}
